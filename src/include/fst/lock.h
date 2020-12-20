ec01c2f8bf08f52267f3308b28a5283757bb7d5e
Martin Jansche <mjansche@gmail.com>
2020-12-20 23:32:58 +0000

    openfst-1.6.0.tar.gz r2
    SHA-256: c03467951631af3f74a6f33ffd50f04285bc4562f79127afd95785120379d293

--- src/include/fst/lock.h
+++ src/include/fst/lock.h
@@ -1,3 +1,15 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
 // See www.openfst.org for extensive documentation on this weighted
 // finite-state transducer library.
 //
@@ -7,8 +19,6 @@
 #define FST_LIB_LOCK_H_
 
 #include <mutex>
-
-#include <fst/compat.h>  // for DISALLOW_COPY_AND_ASSIGN
 
 namespace fst {
 
@@ -25,7 +35,8 @@ class Mutex {
  private:
   std::mutex mu_;
 
-  DISALLOW_COPY_AND_ASSIGN(Mutex);
+  Mutex(const Mutex &) = delete;
+  Mutex &operator=(const Mutex &) = delete;
 };
 
 class MutexLock {
@@ -37,7 +48,8 @@ class MutexLock {
  private:
   Mutex *mu_;
 
-  DISALLOW_COPY_AND_ASSIGN(MutexLock);
+  MutexLock(const MutexLock &) = delete;
+  MutexLock &operator=(const MutexLock &) = delete;
 };
 
 // Currently, we don't use a separate reader lock.
